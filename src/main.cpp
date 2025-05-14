6d12dbfdc38a8b7ca89abc1f28b99a1241bc01f2
Stars <j1325390693@163.com>
2025-05-14 17:16:34 +0800

    [comment]main.cpp 添加注释以解释ImageTransport对象和初始化过程

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "laserMapping");
   ros::NodeHandle nh;
-  image_transport::ImageTransport it(nh);
+  image_transport::ImageTransport it(nh);//创建一个ImageTransport对象it，用于处理ROS中的图像消息。
   LIVMapper mapper(nh); 
-  mapper.initializeSubscribersAndPublishers(nh, it);
+  mapper.initializeSubscribersAndPublishers(nh, it);//初始化该类的订阅者和发布者
   mapper.run();
   return 0;
 }
